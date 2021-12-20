252e1b84aeb15f22fe618a563248b799f5e696f8
Hain Thit <hainlthit@gmail.com>
2021-12-20 08:33:24 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target["new-task-description"].value)
+    form.reset()
+  })
+})
+
+function buildToDo(tasks) {
+  console.log(tasks)
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'click me when completed'
+  li.textContent = `${tasks}   `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
